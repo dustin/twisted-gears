804a2f30dc041fcf615de7473e278cee33ddcebc
Dustin Sallings <dustin@spy.net>
2009-07-23 23:56:43 -0700

    Use StatefulProtocol

--- gearman/client.py
+++ gearman/client.py
@@ -6,48 +6,20 @@ import struct
 
 from collections import deque
 
-from twisted.internet import defer, protocol
+from twisted.internet import defer
+from twisted.protocols import stateful
 from twisted.python import log
 
 from constants import *
 
-__all__ = ['Buffer', 'GearmanProtocol']
+__all__ = ['GearmanProtocol']
 
-class Buffer(deque):
-    """A read buffer that will treat chunks of data as a single string."""
-
-    def strLength(self):
-        """Get the length of the bytes in the string represented herein."""
-        return sum([len(x) for x in self])
-
-    def popBytes(self, n):
-        """Remove n of the first bytes from the buffer."""
-        stuff = []
-        while sum([len(x) for x in stuff]) < n:
-            stuff.append(self.popleft())
-
-        stuff = ''.join(stuff)
-        assert len(stuff) >= n
-
-        # May need to shove some stuff back into the data buffer.
-        if len(stuff) > n:
-            self.appendleft(stuff[n:])
-            stuff = stuff[:n]
-
-        return stuff
-
-    def __str__(self):
-        rv = ''.join(self)
-        return rv
-
-class GearmanProtocol(protocol.Protocol):
+class GearmanProtocol(stateful.StatefulProtocol):
     """Base protocol for handling gearman connections."""
 
-    receivingCommand = 0
-    toread = 0
-
-    def __init__(self):
-        self.current_data = Buffer()
+    def makeConnection(self, transport):
+        stateful.StatefulProtocol.makeConnection(self, transport)
+        self.receivingCommand = 0
         self.deferreds = deque()
 
     def _send_raw(self, cmd, data):
@@ -63,6 +35,9 @@ class GearmanProtocol(protocol.Protocol):
         self.deferreds.append(d)
         return d
 
+    def getInitialState(self):
+        return self._headerReceived, HEADER_LEN
+
     def connectionLost(self, reason):
         try:
             for d in self.deferreds:
@@ -70,11 +45,16 @@ class GearmanProtocol(protocol.Protocol):
         except:
             log.err()
         self.deferreds.clear()
-        self.current_data.clear()
 
-    def _headerReceived(self, cmd, size):
+    def _headerReceived(self, header):
+        if header[:4] != RES_MAGIC:
+            log.msg("Invalid header magic returned, failing.")
+            self.transport.loseConnection()
+            return
+        cmd, size = struct.unpack(">II", header[4:])
+
         self.receivingCommand = cmd
-        self.toread = size
+        return self._completed, size
 
     def _completed(self, data):
         # NOOPs don't *do* anything.
@@ -82,28 +62,8 @@ class GearmanProtocol(protocol.Protocol):
             d = self.deferreds.popleft()
             d.callback(data)
         self.receivingCommand = 0
-        self.toread = 0
 
-    def dataReceived(self, data):
-        self.current_data.append(data)
-        more = True
-
-        while more:
-            if self.receivingCommand:
-                if self.current_data.strLength() >= self.toread:
-                    data = self.current_data.popBytes(self.toread)
-                    self._completed(data)
-            else:
-                if self.current_data.strLength() >= HEADER_LEN:
-                    header = self.current_data.popBytes(HEADER_LEN)
-                    if header[:4] != RES_MAGIC:
-                        log.msg("Invalid header magic returned, failing.")
-                        self.transport.loseConnection()
-                        return
-                    cmd, size = struct.unpack(">II", header[4:])
-                    self._headerReceived(cmd, size)
-                else:
-                    more = False
+        return self._headerReceived, HEADER_LEN
 
     def echo(self, data="hello"):
         """Send an echo request."""
